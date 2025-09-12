e0c15f0f80aaef2c1d290ad91146635317472208
servajean loic <loic.servajean@ynov.com>
2025-09-12 17:05:19 +0200

    main

--- TourparTour/TPT.go
+++ TourparTour/TPT.go
@@ -1,3 +1,19 @@
-Package TPT
+package TPT
 
+type Monster struct {
+	Name  string
+	PVMax int
+	PV    int
+	def   int
+	Atk   int
+}
 
+func initGoblin() Monster {
+	return Monster{
+		Name:  "Goblin",
+		PVMax: 10,
+		PV:    10,
+		def:   1,
+		Atk:   3,
+	}
+}
--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
-bonjour 
+import (
+	TPT "RED/TourparTour"
+	"fmt"
+)
+
+func main() {
+	goblin := TPT.InitGoblin()
+
+	fmt.Println("=== Gobelin ===")
+	fmt.Printf("Nom     : %s\n", goblin.Name)
+	fmt.Printf("PV      : %d / %d\n", goblin.PV, goblin.PVMax)
+	fmt.Printf("Défense : %d\n", goblin.Def)
+	fmt.Printf("Attaque : %d\n", goblin.Atk)
+}
