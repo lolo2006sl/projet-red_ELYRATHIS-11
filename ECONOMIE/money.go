cb7892730270e574a815c26da66cebac26019c8a
Nicolas-CAILLOL <nicolas.caillol@ynov.com>
2025-09-12 17:04:35 +0200

    start money opération

--- ECONOMIE/money.go
+++ ECONOMIE/money.go
@@ -2,4 +2,37 @@ package bank
 
 var money int = 100
 
-func GetMoney() int
+type Objet_Achetable struct {
+	Potion_de_vie int
+}
+
+func GetMoney() int {
+	if BadExpr;
+
+	//Ajoutez un attribut à votre structure Character qui représentera
+	//l’argent du joueur. Donner au joueur 100 pièces d’or au départ
+
+	//Modifier votre marchand lorsque le joueur choisit les items suivants :
+	//« Potion de vie » : le joueur perd 3 pièces d’or
+	//« Potion de poison » : le joueur perd 6 pièces d’or
+	//« Livre de Sort : Boule de feu » : le joueur perd 25 pièces d’or
+	//Ajouter au menu de vente du marchand les items suivants :
+	//« Fourrure de Loup » : le joueur perd 4 pièces d’or
+	//« Peau de Troll » : le joueur perd 7 pièces d’or
+	//« Cuir de Sanglier » : le joueur perd 3 pièces d’or
+	//« Plume de Corbeau » : le joueur perd 1 pièce d’or
+	//Si le joueur choisit un item, il est ajouté à l’inventaire et le coût en pièce d’or est déduit de sa bourse
+	//d’argent.
+
+	//Ajouter au menu principal le choix « Forgeron ». Lorsque le joueur choisit « Forgeron », il doit arriver
+	//dans un autre menu à choix qui va lui proposer la liste d’équipements à fabriquer suivante :
+	//Chapeau de l’aventurier
+	//Tunique de l’aventurier
+	//Bottes de l’aventurier
+	//Si le joueur choisit un objet à fabriquer (et qu’il peut le fabriquer), il perd 5 pièces d’or puis
+	//l’équipement est ajouté à son inventaire.
+
+	//recyclage
+	BadExpr {
+	}
+}
