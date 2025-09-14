473a3b7d6c4ea944bb97df5a4cc4688aeb12fc27
servajean loic <loic.servajean@ynov.com>
2025-09-14 12:55:15 +0200

    TourparTour

--- TourparTour/TPT.go
+++ TourparTour/TPT.go
@@ -2,6 +2,34 @@ package TourparTour
 
 import "fmt"
 
+// ----- STRUCTURE DU HEROS -----
+//a changer surement
+type Hero struct {
+	Name  string
+	PVMax int
+	PV    int
+	Def   int
+	Atk   int
+}
+
+func InitFakeHero() Hero {
+	return Hero{
+		Name:  "Héros",
+		PVMax: 50,
+		PV:    50,
+		Def:   3,
+		Atk:   6,
+	}
+}
+
+func (h Hero) CalculateDamage(targetDef int) int {
+	if targetDef <= 0 {
+		targetDef = 1
+	}
+	return int(float64(h.Atk) / float64(targetDef))
+}
+
+// ----- STRUCTURE DU MONSTRE -----
 type Monster struct {
 	Name  string
 	PVMax int
@@ -13,24 +41,21 @@ type Monster struct {
 func InitGoblin() Monster {
 	return Monster{
 		Name:  "Goblin",
-		PVMax: 10,
-		PV:    10,
+		PVMax: 40,
+		PV:    40,
 		Def:   2,
-		Atk:   3,
+		Atk:   5,
 	}
 }
 
-// Affiche les PV du monstre
+// Calcul des dégats
+func (m Monster) CalculateDamage(targetDef int) int {
+	if targetDef <= 0 {
+		targetDef = 1
+	}
+	return int(float64(m.Atk) / float64(targetDef))
+}
+
 func (m Monster) DisplayHP() {
 	fmt.Printf("%s : %d / %d PV\n", m.Name, m.PV, m.PVMax)
-}
-
-// Calcul des dégâts
-func (m Monster) CalculateDamage() int {
-	heroDef := 2 // Défense temporaire du héros
-	if heroDef == 0 {
-		heroDef = 1
-	}
-	damage := float64(m.Atk) / float64(heroDef)
-	return int(damage)
 }
--- main.go
+++ main.go
@@ -2,15 +2,56 @@ package main
 
 import (
 	"RED/TourparTour"
+	"fmt"
 )
 
 func main() {
+	hero := TourparTour.InitFakeHero()
 	goblin := TourparTour.InitGoblin()
 
-	goblin.DisplayHP()
+	var choix int
+	round := 1
 
-	damage := goblin.CalculateDamage()
-	println("Dégâts infligés par le gobelin :", damage)
+	for hero.PV > 0 && goblin.PV > 0 {
+		fmt.Println()
+		fmt.Println("Tour", round)
 
-	goblin.DisplayHP()
+		// choix du joueur
+		for {
+			fmt.Println("Tape 1 pour attaquer")
+			fmt.Scanln(&choix)
+
+			if choix == 1 {
+				break
+			}
+		}
+
+		// Héros attaque
+		damageToGoblin := hero.CalculateDamage(goblin.Def)
+		goblin.PV -= damageToGoblin
+		if goblin.PV < 0 {
+			goblin.PV = 0
+		}
+		fmt.Println(hero.Name, "attaque", goblin.Name, "et inflige", damageToGoblin, "dégâts") //print les dégats
+
+		// Gobelin attaque
+		damageToHero := goblin.CalculateDamage(hero.Def)
+		hero.PV -= damageToHero
+		if hero.PV < 0 {
+			hero.PV = 0
+		}
+		fmt.Println(goblin.Name, "attaque", hero.Name, "et inflige", damageToHero, "dégâts") //print les dégats
+
+		// Affichage des PV
+		fmt.Println("PV", hero.Name, ":", hero.PV, "| PV", goblin.Name, ":", goblin.PV)
+
+		round++
+	}
+
+	fmt.Println()
+	if hero.PV > 0 {
+		fmt.Println("Victoire du héros !")
+	} else {
+		fmt.Println("Le gobelin a gagné...")
+	}
 }
