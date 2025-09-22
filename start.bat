c92c09a14024151e4b79f1824695fc38d5481a10
servajean loic <loic.servajean@ynov.com>
2025-09-22 09:36:55 +0200

    bat

---
+++ start.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Lancement du jeu. 
+timeout /t 1 >nul
+cls
+echo Lancement du jeu..
+timeout /t 1 >nul
+cls
+echo Lancement du jeu... 
+timeout /t 1 >nul
+cls
+echo Lancement du jeu....
+timeout /t 1 >nul
+go run main.go
